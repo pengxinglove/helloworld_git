f8af2894d638ea153635239645de2587c5826014
pengxing <pengxinglove@qq.com>
2015-09-04 11:42:33 +0800

    change return type

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # helloworld_git
 this is my first opensource in github
 add main.c file on 2015.9.4
+change return type
--- main.c
+++ main.c
@@ -5,22 +5,26 @@
 
 */
 
+/*
+	change return type
+*/
 int div(int a, int b)
 {
 	if(b == 0) return 0;
 	return a / b;
 }
 
-void mul(int a , int b)
+int mul(int a , int b)
 {
 	return  a * b;
 }
-void sub(int a, int b)
+
+int sub(int a, int b)
 {
 	return a - b;
 }
 
-void add(int a, int b)
+int add(int a, int b)
 {
 	return a + b;
 }
