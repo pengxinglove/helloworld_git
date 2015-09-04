29189aa879b83232e396d71e1b8721b11a710b0b
pengxing <pengxinglove@qq.com>
2015-09-04 10:59:17 +0800

    add main.c

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # helloworld_git
 this is my first opensource in github
+add main.c file on 2015.9.4
---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/*
+	this is hello world 
+	author pengxing
+
+*/
+void sub(int a, int b)
+{
+	return a - b;
+}
+
+void add(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	printf("Version 0.1");
+	/*
+		delete printf, because it is not writen on ver2 branch
+	*/
+	return 0;
+}
